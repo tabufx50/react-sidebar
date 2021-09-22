31dfc83c4f326be5f03370c0b7a3d94a7b75de6a
tabufx50 <44902903+tabufx50@users.noreply.github.com>
2021-09-22 13:57:31 -0500

    added new files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Reports from './pages/Reports';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/reports' component={Reports} />
+            <Route path='/products' component={Products} />
+          </Switch>
+        </Router> 
+    </>
   );
 }
 
 export default App;
+
+
+
